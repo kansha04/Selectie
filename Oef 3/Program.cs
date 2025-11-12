11181e266d753f0bf1c229359f7ee59fe7ec14d7
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-12 16:38:54 +0100

    Add Oef 3 project with BMI calculator implementation

---
+++ Oef 3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Maak een BMI calculator die:
+/*  
+    1. Gewicht (kg) en lengte (m) vraagt
+    2. BMI berekent: BMI = gewicht / (lengte * lengte)
+    3. Advies geeft op basis van BMI:
+    < 18.5: "Ondergewicht"
+    18.5 - 24.9: "Normaal gewicht"
+    25 - 29.9: "Overgewicht"
+    = 30: "Obesitas"
+*/
+
+// Variabelen // 
+double gewicht, lengte, bmi;
+string advies;
+// Input //
+Console.WriteLine("Hoeveel weeg je in kg? (bv. 60.2): ");
+gewicht = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Hoe lang ben je in m? (bv. 1.8)");
+lengte = Convert.ToDouble(Console.ReadLine());
+
+// BMI Berekenen //
+bmi = Math.Round(gewicht / (lengte * lengte) * 100, 1);
+Console.WriteLine(bmi);
+
+// Advies Conditional // 
+if (bmi < 18.5)
+{
+    advies = "Ondergewicht";
+}
+else if (bmi >= 18.5 && bmi < 25)
+{
+    advies = "Normaal gewicht";
+}
+else if (bmi >= 25 && bmi < 30)
+{
+    advies = "Overgewicht";
+}
+else
+{
+    advies = "Obesitas";
+}
+
+// Output //
+Console.Clear();
+Console.WriteLine($"Je weegt {gewicht}kg en bent {lengte}m lang en je bmi-score is {bmi}, je bent {advies}");
