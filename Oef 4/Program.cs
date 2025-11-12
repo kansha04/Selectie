8811cdbac72b0fbd0f6cc490be42fba6bc3aed94
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-12 16:53:43 +0100

    Add Oef 4 project with a simple login system implementation

---
+++ Oef 4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Maak een simpel login systeem: //
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Variabelen //
+        string gebruikersnaam = "admin";
+        string wachtwoord = "geheim123";
+        string inputgebruiker, inputww;
+
+        // Login Input // 
+        Console.WriteLine("Gebruikersnaam: ");
+        inputgebruiker = Console.ReadLine();
+        Console.WriteLine("Wachtwoord: ");
+        inputww = Console.ReadLine();
+
+        // Conditional // 
+        if (gebruikersnaam == inputgebruiker && wachtwoord == inputww)
+        {
+            Console.WriteLine("Welkom, admin!");
+        }
+        else if (gebruikersnaam != inputgebruiker && wachtwoord == inputww)
+        {
+            Console.WriteLine("Onbekende gebruiker");
+        }
+        else if (gebruikersnaam == inputgebruiker && wachtwoord != inputww)
+        {
+            Console.WriteLine("Incorrect wachtwoord");
+        }
+        else
+        {
+            System.Console.WriteLine("Error! Try again later.");
+        }
+    }
+}
