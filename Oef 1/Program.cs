1d7fcc1f4323e006323d8da24356b4dee21c6d98
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-12 14:26:30 +0100

    Add Oef 1 project and implement even/odd number checker

---
+++ Oef 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Schrijf een programma dat de gebruiker vraagt om een getal en controleert of het even of oneven is.
+
+// Variabelen
+int getal;
+
+// Input //
+Console.WriteLine("Geef een getal");
+getal = Convert.ToInt32(Console.ReadLine());
+
+// Conditional //
+if (getal % 2 == 0)
+{
+    System.Console.WriteLine("Uw getal is even!");
+} else
+{
+    System.Console.WriteLine("Uw getal is oneven!");
+}
