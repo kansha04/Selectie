673b394bbfbbab81072cbe3ced75ca8cf4f1c620
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-12 14:44:15 +0100

    Add Oef 2 project with functionality to find the largest of three numbers

---
+++ Oef 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Vraag de gebruiker om drie getallen in te voeren en toon het grootste getal. //
+
+// Variabelen //
+double no1, no2, no3;
+
+// Input //
+Console.WriteLine("Getal 1: ");
+no1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Getal 2: ");
+no2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Getal 3: ");
+no3 = Convert.ToDouble(Console.ReadLine());
+
+// Conditionals //
+
+if (no1 > no2 && no1 > no3)
+{
+    System.Console.WriteLine(no1);
+}
+else if (no2 > no1 && no2 > no3)
+{
+    System.Console.WriteLine(no2);
+}
+else
+{
+    System.Console.WriteLine(no3);
+}
