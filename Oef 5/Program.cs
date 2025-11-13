35f20647ec7fb416a10680e9b9898c3d36b5d7ab
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-13 10:39:00 +0100

    Add Oef 5 project with ticket price calculator implementation

---
+++ Oef 5/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Maak een ticket prijs calculator voor een bioscoop:
+Basis prijs: €12
+Kinderen (< 12): €7
+Studenten (met studentenkaart): €9
+Senioren (65+): €8
+VIP leden: 20% korting op hun prijs */
+
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Variabelen //
+        double basisPrijs = 12.0;
+        double kindPrijs = 7.0;
+        double studentPrijs = 9.0;
+        double senioren = 8.0;
+        double vipPrijs = 0.2;
+        int leeftijd;
+        bool studentenkaart, vip;
+
+        // Input //
+        Console.WriteLine("Hoe oud ben je?: ");
+        leeftijd = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ben je een student met een studentenkaart? (true or false): ");
+        studentenkaart = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Heb je een VIP lidmaatschaap? (true or false): ");
+        vip = Convert.ToBoolean(Console.ReadLine());
+    }
+}
