3db153ac68ccbb615bcadc4c5d66d7ece9fdf2d5
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-11-14 12:37:33 +0100

    Add Oef 6 project with leap year checker implementation

---
+++ Oef 6/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Een jaar is een schrikkeljaar als:
+Het deelbaar is door 4, EN
+Het NIET deelbaar is door 100, OF het IS deelbaar door 400 */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Variabelen //
+        int jaar;
+
+        // Input //
+        Console.WriteLine("Geef een jaar: (vb. 2004)");
+        jaar = int.Parse(Console.ReadLine());
+
+        // Conditional //
+        if (jaar % 4 == 0 && jaar % 100 != 0 || jaar % 400 == 0)
+        {
+            Console.WriteLine($"Het jaar {jaar} is een schrikkeljaar.");
+        }
+        else
+        {
+            Console.WriteLine($"Het jaar {jaar} is geen schrikkeljaar.");
+        }
+    }
+}
